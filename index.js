0ad5fa37ce042cb0f1ffc1ec47ea1afa646e5383
Akasam79 <emmachukwuebuka2@gmail.com>
2021-05-09 00:45:18 +0100

    another commit

--- index.js
+++ index.js
@@ -76,23 +76,21 @@ app.get("/db", async (req, res) => {
 // });
 
 app.post("/", (req, res) => {
-
-    const client = await pool.connect(
-      {
+  const client = pool.connect(
+    {
       name: req.body.name,
       email: req.body.email,
-      country: req.body.country
-      },
-      (err, result) => { 
-
-      const result =client.query(
-      "INSERT INTO users VALUES (name, email, country)"
-    );
-        if (err)
-          return res.status(500).json({ message: "Internal Server error" });
-        return res.status(200).json({ message: "New client added successfully" });
-      }
-    );
+      country: req.body.country,
+    },
+    (err, result) => {
+      const result = client.query(
+        "INSERT INTO users VALUES (name, email, country)"
+      );
+      if (err)
+        return res.status(500).json({ message: "Internal Server error" });
+      return res.status(200).json({ message: "New client added successfully" });
+    }
+  );
 });
 
 // app.get("/client", (req, res) => {
