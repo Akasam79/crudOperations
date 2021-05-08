5a41c2c8b9ad5b8d00dca5b050642d6009d15791
Akasam79 <emmachukwuebuka2@gmail.com>
2021-05-08 19:00:36 +0100

    commit change

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.get("/db", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query("SELECT * FROM test_table");
+    const result = await client.query("SELECT * FROM users");
     const results = { results: result ? result.rows : null };
     res.render("pages/db", results);
     client.release();
